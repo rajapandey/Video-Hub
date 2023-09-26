dfcbffa5dbd45dc215833688f251cb74c4f1a0d9
Raja <rajapandey@gmail.com>
2023-09-26 22:06:17 +0530

    new repo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import { Box, Stack, VStack, Heading, HStack, Button, Input, Text } from '@chakra-ui/react';
+import React from 'react';
+import {AiOutlineSend} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <Box bgColor={'blackAlpha.900'} minH={'40'} p='16' color={'white'}>
+        <Stack direction={['column', 'row']}>
+            <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+                <Heading size='md' textTransform={'uppercase'} textAlign={['center','left']}>
+                    Subscibe to get updates
+                </Heading>
+                <HStack borderBottom={'2px solid white'} py={'2'}>
+                    <Input placeholder='Enter Email Here...' border={'none'} borderRadius={'none'} outline={'none'} focusBorderColor='none'/>
+                    <Button p={'0'} colorScheme={'purple'} variant={'ghost'} borderRadius={'0 20px 20px 0'}>
+                        <AiOutlineSend size={'20'}/>
+                    </Button>
+                </HStack>
+            </VStack>
+
+            <VStack w={'full'} borderLeft={['none', '1px solid white']} borderRight={['none', '1px solid white']}>
+                <Heading textTransform={'uppercase'} textAlign={'center'}>
+                    Video Hub
+                </Heading>
+                <Text>All rights reserved</Text>
+            </VStack>
+            <VStack w={'full'}>
+                <Heading size={'md'} textTransform={'uppercase'}>Social media</Heading>
+                <Button variant={'link'} colorScheme={'white'}><a href='https://youtube.com' target='blank'>Youtube</a></Button>
+                <Button variant={'link'} colorScheme={'white'}><a href='https://instagram.com' target='blank'>Instagram</a></Button>
+                <Button variant={'link'} colorScheme={'white'}><a href='https://github.com' target='blank'>Github</a></Button>
+            </VStack>
+
+        </Stack>
+    </Box>
+  )
+}
+
+export default Footer;
